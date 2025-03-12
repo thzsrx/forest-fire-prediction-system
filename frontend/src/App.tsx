8933dd2e467c24aeb8b35a49972c7c50372ef7fc
Subhamranjan <subhamranjansahoo1234@gmail.com>
2025-03-12 11:00:24 +0530

    updates files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
+import Layout from './components/Layout';
+import { ThemeProvider } from './context/theme-provider';
+import WeatherDashboard from './pages/weather-dashboard';
+import CityPage from './pages/city-page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 5 * 60 * 1000, 
+            gcTime: 10 * 60 * 1000,
+            retry: false,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeProvider defaultTheme='dark'>
+                    <Layout>
+                        <Routes>
+                            <Route path='/' element={<WeatherDashboard />}/>
+                            <Route path='/city/:cityName' element={<CityPage />}/>
+                        </Routes>
+                    </Layout>
+                </ThemeProvider>
+            </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+}
+
+export default App
